081dfa8532522f3a8e0e3f382101ab7eaafa335a
Demis Bellot <demis.bellot@gmail.com>
2014-07-25 04:16:57 -0400

    Working Chat on First Commit!

---
+++ lib/copy.bat
@@ -0,0 +1,1 @@
+COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
---
+++ src/Chat/Global.asax.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+using Funq;
+using ServiceStack;
+using ServiceStack.Razor;
+using ServiceStack.Text;
+
+namespace Chat
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("Chat", typeof(ServerEventsService).Assembly) {}
+
+        public override void Configure(Container container)
+        {
+            JsConfig.EmitCamelCaseNames = true;
+            Plugins.Add(new RazorFormat());
+            Plugins.Add(new ServerEventsFeature());
+        }
+    }
+
+    [Route("/channels/{Channel}/chat")]
+    public class PostChatToChannel : IReturn<ChatMessage>
+    {
+        public string From { get; set; }
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string Selector { get; set; }
+    }
+
+    public class ChatMessage
+    {
+        public long Id { get; set; }
+        public string FromUserId { get; set; }
+        public string FromName { get; set; }
+        public string DisplayName { get; set; }
+        public string Message { get; set; }
+        public string UserAuthId { get; set; }
+        public bool Private { get; set; }
+    }
+
+    [Route("/channels/{Channel}/raw")]
+    public class PostRawToChannel : IReturnVoid
+    {
+        public string From { get; set; }
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string Selector { get; set; }
+    }
+
+    public class ServerEventsService : Service
+    {
+        private static long msgId;
+
+        public IServerEvents ServerEvents { get; set; }
+
+        public object Any(PostChatToChannel request)
+        {
+            var sub = ServerEvents.GetSubscription(request.From);
+            if (sub == null)
+                throw HttpError.NotFound("Subscription {0} does not exist".Fmt(request.From));
+
+            var msg = new ChatMessage
+            {
+                Id = Interlocked.Increment(ref msgId),
+                FromUserId = sub.UserId,
+                FromName = sub.DisplayName,
+                Message = request.Message,
+            };
+
+            if (request.ToUserId != null)
+            {
+                msg.Private = true;
+                ServerEvents.NotifyUserId(request.ToUserId, request.Selector, msg);
+                var toSub = ServerEvents.GetSubscriptionByUserId(request.ToUserId);
+                if (toSub != null)
+                {
+                    msg.Message = "@{0}: {1}".Fmt(toSub.DisplayName, msg.Message);
+                    ServerEvents.NotifySubscription(request.From, request.Selector, msg);
+                }
+            }
+            else
+            {
+                ServerEvents.NotifyChannel(request.Channel, request.Selector, msg);
+            }
+
+            return msg;
+        }
+
+        public void Any(PostRawToChannel request)
+        {
+            var sub = ServerEvents.GetSubscription(request.From);
+            if (sub == null)
+                throw HttpError.NotFound("Subscription {0} does not exist".Fmt(request.From));
+
+            if (request.ToUserId != null)
+            {
+                ServerEvents.NotifyUserId(request.ToUserId, request.Selector, request.Message);
+            }
+            else
+            {
+                ServerEvents.NotifyChannel(request.Channel, request.Selector, request.Message);
+            }
+        }
+    }
+
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+    }
+}
