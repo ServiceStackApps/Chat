78b23326e387a3aa649db29b7e4fb2c7099122ca
Demis Bellot <demis.bellot@gmail.com>
2017-03-27 03:18:18 -0400

    Update to latest SS .dlls

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,2 +1,6 @@
-COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
-COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Release\* .
+SET BUILD=Release
+REM SET BUILD=Debug
+
+COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\%BUILD%\* .
+COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\* .
+COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\%BUILD%\ServiceStack.Authentication.OAuth2.dll .
