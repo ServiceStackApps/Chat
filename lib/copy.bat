21b15ab645950fc5750520d6312b305d4d2908b1
Demis Bellot <demis.bellot@gmail.com>
2017-12-18 00:54:03 -0500

    Upgrade to ServiceStack v5

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,6 +1,6 @@
 SET BUILD=Release
 REM SET BUILD=Debug
 
-COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\%BUILD%\* .
-COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\* .
-COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\%BUILD%\ServiceStack.Authentication.OAuth2.dll .
+COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\%BUILD%\net45\* .
+COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\net45\* .
+COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\%BUILD%\net45\ServiceStack.Authentication.OAuth2.dll .
